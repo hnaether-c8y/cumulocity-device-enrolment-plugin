40dd797b67a030e80c00a643d3d670b78794aee6
Reuben Miller <reuben.d.miller@gmail.com>
2024-06-01 12:33:29 +0200

    remove unused code

--- asset-properties-widget/asset-properties-widget.module.ts
+++ asset-properties-widget/asset-properties-widget.module.ts
@@ -10,30 +10,16 @@ import {
 } from '@c8y/ngx-components';
 import * as preview from './common/preview';
 import { AssetPropertiesViewComponent } from './component/asset-properties-view/asset-properties-view.component';
-import { AssetPropertiesConfigComponent } from './component/asset-properties-config/asset-properties-config.component';
 import { AssetSelectorModule } from '@c8y/ngx-components/assets-navigator';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { AssetPropertiesComponent } from './component/asset-properties-view/asset-properties/asset-properties.component';
-import { AssetPropertiesItemComponent } from './component/asset-properties-view/asset-properties/asset-properties-item.component';
-import { assetPropertyItemSelectorCtrlComponent } from './component/asset-properties-config/asset-property-item-selector/asset-property-item-selector.component';
-import { AssetPropertiesSelectorComponent } from './component/asset-properties-config/asset-property-selector/asset-property-selector.component';
 import { SubAssetsModule } from '@c8y/ngx-components/sub-assets';
-import { ComputedPropertyConfigComponent } from './component/asset-properties-config/computed-asset-property/computed-property-config.component';
 import { DatapointSelectorModule } from '@c8y/ngx-components/datapoint-selector';
-import { ComputedPropertyLastMeasurementConfigComponent } from './component/asset-properties-config/computed-asset-property/properties/last-measurement-config.component';
 import { AssetPropertiesViewService } from './component/asset-properties-view/asset-properties-view.service';
 
 @NgModule({
   declarations: [
-    AssetPropertiesConfigComponent,
-    AssetPropertiesSelectorComponent,
-    assetPropertyItemSelectorCtrlComponent,
     AssetPropertiesViewComponent,
-    AssetPropertiesComponent,
-    AssetPropertiesItemComponent,
-    ComputedPropertyConfigComponent,
-    ComputedPropertyLastMeasurementConfigComponent
   ],
   imports: [
     CoreModule,
@@ -55,11 +41,10 @@ import { AssetPropertiesViewService } from './component/asset-properties-view/as
       multi: true,
       useValue: {
         id: 'asset-properties-widget',
-        label: 'Asset Properties 2.0',
+        label: 'Device Enrolment',
         previewImage: preview.image,
-        description: 'Editable form for asset properties widget',
+        description: 'Enrol new devices using a one-time code',
         component: AssetPropertiesViewComponent,
-        configComponent: AssetPropertiesConfigComponent,
         data: {
           ng1: {
             options: {
